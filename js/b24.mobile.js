5d384117b4d996a86cd8273d3f1d2542d9e0faf8
Phys <phys@aic.ru>
2014-09-29 20:39:59 +0400

    B24
    Signed-off-by: Phys <phys@aic.ru>

---
+++ js/b24.mobile.js
@@ -0,0 +1,52 @@
+// Inits
+$(document).ready(function() {
+  SCarousel();
+  PopUp();
+  SForm();
+  MMenu();
+});
+
+
+// Popup
+function PopUp() {
+  $('.info').on('click', function(){
+    $('#popup').show();
+  }); 
+
+  $('#popup .close').on('click', function(){
+    $('#popup').hide();
+  });
+  
+  $(document).keyup(function(d) {
+    if (d.keyCode == 27) {
+      $('#popup').hide();
+    }
+  });        
+}
+
+// Main menu
+function MMenu() {
+  $('#header .menu_link').on('click', function(){ 
+    if ($(this).hasClass('closed')) {     
+      $(this).addClass('opened').removeClass('closed');  
+      $('#header, #cover, #footer').animate({left:'66%'});
+      $('.menu_cover').animate({left:0});       
+    } else { 
+      $(this).addClass('closed').removeClass('opened');
+      $('#header, #cover, #footer').animate({left:0});
+      $('.menu_cover').animate({left:'-66%'});       
+    } 
+  });                                                           
+}
+
+// Carousel
+function SCarousel() {
+    $('.slider').slick({
+        dots:true
+    });                                                         
+}
+
+// Styler
+function SForm() {
+    $('select').styler();     
+}
