d6cb1f0078bf3494a3cb61102680831841119bf9
Phys <phys@aic.ru>
2014-09-30 22:01:59 +0400

    Правки
    Signed-off-by: Phys <phys@aic.ru>

--- js/b24.mobile.js
+++ js/b24.mobile.js
@@ -13,11 +13,19 @@ function THeader() {
 
   $(window).on('scroll', function(){ 
 
-    var wTop = ($(document).scrollTop())-1;
-    
-    $('#header').css({top: wTop}); 
+    var wTop = ($(document).scrollTop())-1,
+        tTop = ($('#b24_price #row_2').offset().top)-115;
+
+    $('#header').css({top: wTop});
+
+    $('#drag_cover .fixed').css({top: wTop});
 
 
+    if (wTop>tTop){ 
+      $('#toggle_cover').css({top: wTop});
+    } else {
+      $('#toggle_cover').css({top: 'auto'});
+    }
   }); 
 
  
@@ -52,20 +60,23 @@ function STable() {
   $('#toggle div').draggable({
     stop: function(){
       var posL = $(this).position().left,
-          widthE = ($('#toggle').width())/4;
+          widthE = ($('#toggle').width())/4,
+          center = $('#drag_cover table').position().left;
 
       if (posL>widthE) { 
         $('#toggle span').removeClass('active');
         $('#toggle .box').addClass('active');   
         $('#toggle div').animate({left: 'none', right:'5px'}, 200); 
         $('#drag_cover table').animate({left:'0'}, 200); 
+        $('#drag_cover .centred DIV').animate({left:center},200);
       } else {
         $('#toggle span').removeClass('active');
         $('#toggle .cloud').addClass('active');   
         $('#toggle div').animate({right:'none', left:'5px'}, 200); 
-        $('#drag_cover table').animate({left: '33.33333%'}, 200);               
+        $('#drag_cover table').animate({left: '33.33333%'}, 200); 
+        $('#drag_cover .centred DIV').animate({left:center},200);              
       } 
-    },
+    },    
     containment:'#toggle',
     axis: 'x',
     scroll:false
@@ -75,19 +86,21 @@ function STable() {
     stop: function(){
 
       var posL = $(this).position().left,
-          widthE = ($('#drag_cover').width())/6;
+          widthE = ($('#drag_cover').width())/6,
+          center = $('#drag_cover table').position().left;
       if (posL>widthE) { 
         $('#toggle span').removeClass('active');
         $('#toggle .box').addClass('active');   
         $('#toggle div').animate({left: 'none', right:'5px'}, 200); 
         $(this).animate({left:'0'}, 200); 
+        $('#drag_cover .centred DIV').animate({left:center},200); 
       } else {
         $('#toggle span').removeClass('active');
         $('#toggle .cloud').addClass('active');   
         $('#toggle div').animate({right:'none', left:'5px'}, 200); 
-        $(this).animate({left: '33.33333%'}, 200);        
-      } 
-
+        $(this).animate({left: '33.33333%'}, 200);
+        $('#drag_cover .centred DIV').animate({left:center},200);         
+      }
     },
     containment:'#drag_cover', 
     axis: 'x'
@@ -104,6 +117,9 @@ function STable() {
       $('#toggle div').animate({right:'none', left:'5px'});
       $('#drag_cover table').animate({left: '33.33333%'});           
     } 
+
+    var center = $('#drag_cover table').position().left;
+    $('#drag_cover .centred DIV').animate({left:center});
   }); 
  
 }
